e29cef52beb97bb3193e003e55bad1758ae7dc1d
Letera Tujo <168364436+letera1@users.noreply.github.com>
2025-06-29 15:28:26 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Products from "./components/Products/Products";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import TopProducts from "./components/TopProducts/TopProducts";
-import Banner from "./components/Banner/Banner";
+import Banner from "./components/Bannner/Banner";
 import Subscribe from "./components/Subscribe/Subscribe";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Footer from "./components/Footer/Footer";
