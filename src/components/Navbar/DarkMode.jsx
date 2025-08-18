3bcbd09c451a8f252c12de6f1e6755a4c6068667
Letera Tujo <168364436+letera1@users.noreply.github.com>
2025-08-18 20:42:39 +0300

    Update DarkMode.jsx

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -23,7 +23,7 @@ const DarkMode = () => {
     <div className="relative">
       <img
         src={LightButton}
-        alt="light button"
+        alt="light-button"
         onClick={() => setTheme(theme === "light" ? "dark" : "light")}
         className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10 ${
           theme === "dark" ? "opacity-0" : "opacity-100"
